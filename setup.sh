f4845eaf3ea6c4ed37e427889cddc98acf1e4a20
Christian Huff <christian.huff@cofinpro.de>
2015-08-28 22:31:52 +0200

    codingdojo-8 Logfile des Wildfly betrachten: Expose port 8080

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ docker build -t="codingdojo/wildfly:latest" wildfly/
 #docker build -t="codingdojo/mariadb:latest" mariadb/
 #docker run -d -p 3306:3306 --name=mariadb codingdojo/mariadb
 #docker run -d -p 9990:9990 --name=wildfly --link mariadb:mariadb codingdojo/wildfly
-docker run -d -p 9990:9990 --name=wildfly codingdojo/wildfly
+docker run -d -p 9990:9990 -p 8080:8080 --name=wildfly codingdojo/wildfly
